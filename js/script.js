752e4d25af5af3dae012a4c85f66962ee4d82f5b
antonin-stephany <stephany.antonin@gmail.com>
2023-04-08 19:33:16 +0200

    anim loop done

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 const observer = new IntersectionObserver((entries)=>{
     for(const entry of entries){
         if(entry.isIntersecting){
-          console.log(entry.target.parentNode.previousSibling.previousSibling)
             entry.target.parentNode.previousSibling.previousSibling.previousSibling.previousSibling.querySelector('.container h2').classList.add('isVisible');
             entry.target.parentNode.previousSibling.previousSibling.querySelector('.line').classList.add('isVisible');
             observer.unobserve(entry.target)
@@ -22,8 +21,10 @@ const itemImgLink = document.querySelectorAll('.img-home')
 for (const item of itemsImg) {
   item.addEventListener("mouseover", function(event){
     event.target.parentNode.previousSibling.previousSibling.classList.add("hoverEffect");
+    event.target.parentNode.previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.querySelector('.text-outline-container').classList.add("hoverEffect");
   });
   item.addEventListener("mouseleave", function(event){
     event.target.parentNode.previousSibling.previousSibling.classList.remove("hoverEffect");
+    event.target.parentNode.previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.querySelector('.text-outline-container').classList.remove("hoverEffect");
   });
 }
