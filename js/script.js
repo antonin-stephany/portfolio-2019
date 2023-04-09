9c2129ba209d8b55ba9557ca4885fa701d415fed
antonin-stephany <stephany.antonin@gmail.com>
2023-04-09 16:40:28 +0200

    anim loop done in all titles

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
+
 //apparition titre premiere visite du site
 const observer = new IntersectionObserver((entries)=>{
     for(const entry of entries){
         if(entry.isIntersecting){
+          //effet sur le titre
             entry.target.parentNode.previousSibling.previousSibling.previousSibling.previousSibling.querySelector('.container h2').classList.add('isVisible');
+            //effet sur la ligne
             entry.target.parentNode.previousSibling.previousSibling.querySelector('.line').classList.add('isVisible');
             observer.unobserve(entry.target)
         }
@@ -20,11 +23,15 @@ for (const item of itemsImg) {
 const itemImgLink = document.querySelectorAll('.img-home');
 for (const item of itemsImg) {
   item.addEventListener("mouseover", function(event){
+    //effet sous titre
     event.target.parentNode.previousSibling.previousSibling.classList.add("hoverEffect");
+    //effet titre loop
     event.target.parentNode.previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.querySelector('.text-outline-container').classList.add("hoverEffect");
   });
   item.addEventListener("mouseleave", function(event){
+    //effet sous titre
     event.target.parentNode.previousSibling.previousSibling.classList.remove("hoverEffect");
+    //effet titre loop
     event.target.parentNode.previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.querySelector('.text-outline-container').classList.remove("hoverEffect");
   });
 }
